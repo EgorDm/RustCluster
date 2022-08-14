0d142521a5375d2e033726a0bebf4bf21497a60a
Egor Dmitriev <egordmitriev2@gmail.com>
2022-08-14 18:14:06 +0200

    feature: Updated synthetic 2d example with plotting and simplified mixture modeling process

--- dpmmsc/examples/synthetic_2d.rs
+++ dpmmsc/examples/synthetic_2d.rs
@@ -12,68 +12,36 @@ use dpmmsc::global::{GlobalActions, GlobalState};
 use dpmmsc::local::{LocalActions, LocalState};
 use dpmmsc::metrics::normalized_mutual_info_score;
 use dpmmsc::options::{FitOptions, ModelOptions};
+use dpmmsc::plotting::{axes_range_from_points, Cluster2D, init_axes2d};
 use dpmmsc::priors::{NIW, NIWParams, NIWStats, SufficientStats};
 
 fn plot<S: Storage<f64, Dynamic, Dynamic>>(
     path: &str,
     points: &Matrix<f64, Dynamic, Dynamic, S>, labels: &[usize], clusters: &[SuperClusterParams<NIW>]
 ) {
-    let (mut min_x, mut max_x) = (f64::INFINITY, f64::NEG_INFINITY);
-    let (mut min_y, mut max_y) = (f64::INFINITY, f64::NEG_INFINITY);
-    for row in points.row_iter() {
-        min_x = min_x.min(row[0]);
-        max_x = max_x.max(row[0]);
-        min_y = min_y.min(row[1]);
-        max_y = max_y.max(row[1]);
-    }
-
     let root = BitMapBackend::new(path, (1024, 768)).into_drawing_area();
-    root.fill(&WHITE).unwrap();
-    let areas = root.split_by_breakpoints([944], [80]);
-    let mut scatter_ctx = ChartBuilder::on(&areas[2])
-        .x_label_area_size(40)
-        .y_label_area_size(40)
-        .build_cartesian_2d(min_x..max_x, min_y..max_y).unwrap();
-    scatter_ctx
-        .configure_mesh()
-        .disable_x_mesh()
-        .disable_y_mesh()
-        .draw().unwrap();
-    scatter_ctx.draw_series(
+    let (mut range_x, mut range_y) = axes_range_from_points(points);
+    // let (mut range_x, mut range_y) = (-50.0..50.0, -50.0..50.0);
+    let mut plot_ctx = init_axes2d((range_x, range_y), &root);
+
+    plot_ctx.draw_series(
         points
             .row_iter()
             .zip(labels.iter())
             .map(|(row, label)|
-                Circle::new((row[0], row[1]), 2, Palette99::pick(*label).mix(0.9).filled(),
-                )),
-    ).unwrap();
-
-    scatter_ctx.draw_series(
-        clusters.iter()
-            .enumerate()
-            .map(|(k, cluster)| {
-                let color = Palette99::pick(k);
-                let circle = Circle::new((cluster.prim.dist.mu()[0], cluster.prim.dist.mu()[1]), 8, color.filled().stroke_width(2));
-                circle
-            }),
+                Circle::new((row[0], row[1]), 2, Palette99::pick(*label).mix(0.9).filled())),
     ).unwrap();
 
     for (k, cluster) in clusters.iter().enumerate() {
-        scatter_ctx.draw_series(LineSeries::new(
-            (0..100).map(|i| {
-                let t = i as f64 / 100.0 * PI * 2.0;
-                let (cx, cy) = (t.cos(), t.sin());
-
-                let x = cluster.prim.dist.cov()[(0, 0)] * cx + cluster.prim.dist.cov()[(0, 1)] * cy + cluster.prim.dist.mu()[0];
-                let y = cluster.prim.dist.cov()[(1, 0)] * cx + cluster.prim.dist.cov()[(1, 1)] * cy + cluster.prim.dist.mu()[1];
-
-                (x, y)
-            }),
-            Palette99::pick(k)
-        )).unwrap();
+        plot_ctx.draw_series(
+            Cluster2D::from_mat(
+                cluster.prim.dist.mu(), cluster.prim.dist.cov(),
+                50,
+                Palette99::pick(k).filled(),
+                Palette99::pick(k).stroke_width(2),
+            )
+        ).unwrap();
     }
-
-
 
     root.present().expect("Unable to write result to file, please make sure 'plotters-doc-data' dir exists under current dir");
     println!("Result has been saved to {}", path);
@@ -92,7 +60,6 @@ fn main() {
     let mut rng = StdRng::seed_from_u64(42);
     let plot_idx: Vec<usize> = (0..1000).map(|_| rng.gen_range(0..x.nrows())).collect();
     let plot_x = x.select_rows(&plot_idx);
-    let plot_y = y.select_rows(&plot_idx);
 
     let dim = x.ncols();
 
@@ -100,7 +67,7 @@ fn main() {
     model_options.alpha = 100.0;
     model_options.outlier = None;
     let mut fit_options = FitOptions::default();
-    fit_options.init_clusters = 2;
+    fit_options.init_clusters = 5;
 
     let mut rng = StdRng::seed_from_u64(fit_options.seed);
     let mut local_state = LocalState::<NIW>::from_init(x, fit_options.init_clusters, &model_options, &mut rng);
@@ -109,13 +76,20 @@ fn main() {
 
     // update_suff_stats_posterior!
     let stats = LocalState::<NIW>::collect_stats(&local_state, global_state.n_clusters());
-    for (k, (prim, aux)) in stats.into_iter().enumerate() {
-        global_state.clusters[k].prim.stats = prim;
-        for (ki, aux) in aux.into_iter().enumerate() {
-            global_state.clusters[k].aux[ki].stats = aux;
-        }
-    }
+    GlobalState::update_clusters_post(&mut global_state, stats);
     GlobalState::update_sample_clusters(&mut global_state, &model_options, &mut rng);
+
+
+    let plot_y = local_state.labels.select_rows(&plot_idx);
+    plot(&format!("examples/data/plot/synthetic_2d/init.png"), &plot_x, plot_y.as_slice(), &global_state.clusters);
+
+    // // Testing !!!
+    // LocalState::update_sample_labels(&global_state, &mut local_state, true, &mut rng);
+    // LocalState::update_sample_labels_aux(&global_state, &mut local_state, &mut rng);
+    //
+    // let plot_y = local_state.labels.select_rows(&plot_idx);
+    // plot(&format!("examples/data/plot/synthetic_2d/test.png"), &plot_x, plot_y.as_slice(), &global_state.clusters);
+    // // End Testing
 
     for i in 0..fit_options.iters {
         let is_final = i >= fit_options.iters - fit_options.argmax_sample_stop;
@@ -126,15 +100,11 @@ fn main() {
         {
             GlobalState::update_sample_clusters(&mut global_state, &model_options, &mut rng);
             LocalState::update_sample_labels(&global_state, &mut local_state, is_final, &mut rng);
+            // LocalState::update_sample_labels(&global_state, &mut local_state, true, &mut rng);
             LocalState::update_sample_labels_aux(&global_state, &mut local_state, &mut rng);
             // update_suff_stats_posterior!
             let stats = LocalState::<NIW>::collect_stats(&local_state, global_state.n_clusters());
-            for (k, (prim, aux)) in stats.into_iter().enumerate() {
-                global_state.clusters[k].prim.stats = prim;
-                for (ki, aux) in aux.into_iter().enumerate() {
-                    global_state.clusters[k].aux[ki].stats = aux;
-                }
-            }
+            GlobalState::update_clusters_post(&mut global_state, stats);
             let bad_clusters = GlobalState::collect_bad_clusters(&mut global_state);
             LocalState::update_reset_clusters(&mut local_state, &bad_clusters, &mut rng);
 
@@ -162,8 +132,13 @@ fn main() {
         let nmi = normalized_mutual_info_score(y.as_slice(), local_state.labels.as_slice());
         println!("Run iteration {} in {:.2?}; k={}, nmi={}", i, elapsed, global_state.n_clusters(), nmi);
 
-        plot(&format!("examples/data/plot/step_{:04}.png", i), &plot_x, plot_y.as_slice(), &global_state.clusters);
 
+        let plot_y = local_state.labels.select_rows(&plot_idx);
+        plot(&format!("examples/data/plot/synthetic_2d/step_{:04}.png", i), &plot_x, plot_y.as_slice(), &global_state.clusters);
+
+        // if i > 2 {
+        //     break;
+        // }
 
         // calculate NMI
     }
